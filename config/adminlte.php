8d3cf55d30e9efd8ecfa6a92f51d8bbd6a70c344
aemestica <alexander.alberto.emestica@gmail.com>
2023-10-09 19:37:47 -0600

    Desarrollo formulario de sucursales

--- config/adminlte.php
+++ config/adminlte.php
@@ -312,86 +312,29 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        [
-            'text'        => 'usuarios',
+            'text'        => 'Usuarios',
             'url'         => 'usuarios',
             'icon'        => 'far fa-fw fa-file',
         ],
         [
-            'text'        => 'productos',
+            'text'        => 'Productos',
             'url'         => 'productos',
             'icon'        => 'far fa-fw fa-file',
         ],
-        ['header' => 'account_settings'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text'        => 'Sucursales',
+            'url'         => 'sucursales',
+            'icon'        => 'far fa-fw fa-file',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text'        => 'Clientes',
+            'url'         => 'clientes',
+            'icon'        => 'far fa-fw fa-file',
         ],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text'        => 'Cobro',
+            'url'         => 'cobro',
+            'icon'        => 'far fa-fw fa-file',
         ],
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,26 @@ use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return view('home');
 });
-
-
-
-
-
 
 Route::get('/usuarios', [
     UsuariosController::class, 'GetUsuario'
 ]);
 
-
 Route::get('/productos', [
     ProductosController::class, 'GetProductos'
+]);
+
+Route::get('/clientes', [
+    ProductosController::class, 'GetClientes'
+]);
+
+Route::get('/sucursales', [
+    ProductosController::class, 'GetSucursales'
+]);
+
+Route::get('/cobro', [
+    ProductosController::class, 'GetCobro'
 ]);
